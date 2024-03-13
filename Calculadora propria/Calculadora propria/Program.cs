fbf453ff025cab3d05098caf1a93be7fde1a045c
gusta <gusta@DESKTOP-GQ6EVQC>
2024-03-13 14:29:13 -0300

    Add project files.

---
+++ Calculadora propria/Calculadora propria/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Diagnostics;
+using System.Reflection.Metadata;
+
+namespace Calculadora_propria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Calculadora propria");
+
+                Console.WriteLine("Digite 1 para adicionar");
+                Console.WriteLine("Digite 2 para subtrair");
+                Console.WriteLine("Digite 3 para dividir");
+                Console.WriteLine("digite 4 para multiplicar");
+                Console.WriteLine("Digite S se quiser sair");
+
+
+                string operacao = Console.ReadLine();
+
+                             {
+                        if (operacao == "S")
+                        {
+                            break;
+                        }
+                        Console.WriteLine();
+
+                        Console.WriteLine("Digite o primeiro numero");
+                        string primeironumerostring = Console.ReadLine();
+                        int primeironumero = Convert.ToInt32(primeironumerostring);
+
+
+                        Console.WriteLine("digite o segundo numero");
+                        string segundonumerostring = Console.ReadLine();
+                        int segundonumero = Convert.ToInt32(segundonumerostring);
+
+
+                        int resultado = 0;
+
+                        if (operacao == "1")
+                        {
+
+                            resultado = primeironumero + segundonumero;
+
+                        }
+                        else if (operacao == "2")
+                        {
+
+                            resultado = primeironumero - segundonumero;
+
+                        }
+                        else if (operacao == "3")
+                        {
+
+                            if (segundonumero <= 0 || primeironumero <= 0)
+                            {
+
+                                Console.Clear();
+                            }
+
+
+                            resultado = primeironumero / segundonumero;
+                            Console.WriteLine("todo numero dividido por 0 e igual a zero");
+                            Console.WriteLine("Aperte enter para voltar ao inicio");
+
+                        }
+
+                      else if (operacao == "4")
+                      { 
+                           resultado = primeironumero * segundonumero;
+                      }
+                        Console.WriteLine("o resultado e " + resultado);
+                        Console.ReadLine();
+
+                    }
+                }
+
+            }
+
+        }
+    }
+
---
+++ Calculadora/Calculadora.ConsoleApp/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics;
+
+namespace Calculadora.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Calculadora");
+                Console.WriteLine("digite 1 para Adicionar");
+                Console.WriteLine("digite 2 para subtrair");
+                Console.WriteLine("digite 3 para multiplicar");
+                Console.WriteLine("digite 4 para dividir");
+
+
+                Console.WriteLine("digite S para sair");
+
+                string operacao = Console.ReadLine();
+
+                if (operacao == "S")
+                {
+                    break;
+                }
+
+                Console.WriteLine("digite o primeiro numero");
+
+                string primeironumeroString = Console.ReadLine();
+                int primeironumero = Convert.ToInt32(primeironumeroString);
+
+                Console.WriteLine(primeironumeroString);
+
+                Console.WriteLine("digite o segundo numero");
+
+                string segundonumeroString = Console.ReadLine();
+                int segundonumero = Convert.ToInt32(segundonumeroString);
+
+
+                int resultado = 0;
+
+                if (operacao == "1")
+                {
+
+                    resultado = primeironumero + segundonumero; 
+                }
+
+                 else if (operacao == "2")
+                {
+
+                    resultado = primeironumero - segundonumero;
+                }
+                else if (operacao == "3")
+                {
+
+                    resultado = primeironumero * segundonumero;
+                }
+                else if (operacao == "4")
+                {
+
+                    resultado = primeironumero / segundonumero;
+                }
+
+
+
+                Console.WriteLine("o resultado e:  " + resultado);
+
+                Console.ReadLine();
+
+
+                Console.WriteLine("voce deseja sair (S/N)");
+
+                string resposta = Console.ReadLine();
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
+
+
+
