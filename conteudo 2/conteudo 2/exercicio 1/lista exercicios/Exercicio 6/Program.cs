9a2e3e5f7bc05f09e41d3c3fac9be3cfa1e02095
gustavo santos <gustavo1912008@gmail.com>
2024-03-14 17:28:35 -0300

    novos exercicios

---
+++ conteudo 2/conteudo 2/exercicio 1/lista exercicios/Exercicio 6/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Exercicio_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.Clear();
+
+
+                Console.WriteLine("escreva a temperatura em celsius");
+
+                string temperatura1 = "";
+
+                temperatura1 = Console.ReadLine();
+                double temperaturaC = Convert.ToDouble(temperatura1);
+
+
+                double resultado1 = temperaturaC * 1.8 ;
+
+                double resultado2 = resultado1 + 32;
+
+
+                Console.WriteLine("A temperatura em Fahrenheit e:" + resultado2);
+
+
+                Console.WriteLine("Digite S para sair e N para voltar ao inicio");
+
+                String sair = Console.ReadLine();
+                if (sair == "S")
+                {
+
+                    break;
+
+                }
+                if (sair == "N")
+                {
+
+                    continue;
+
+
+                }
+            }
+        }
+   }
+    }
+
---
+++ conteudo 2/conteudo 2/exercicio 1/lista exercicios/Exercicio 7/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Exercicio_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Informe seu salario base");
+                String salariobase = "";
+                salariobase = Console.ReadLine();
+                double salariobase2 = Convert.ToDouble(salariobase);
+
+                Console.WriteLine("Informe sua comissao por base do numero de vendas");
+                string comissao = "";
+                comissao = Console.ReadLine();
+                    double comissao2=Convert.ToDouble(comissao); 
+
+                Console.WriteLine("Informe seu total de vendas do mes");
+                string totaldevendas = "";
+                totaldevendas = Console.ReadLine();
+                double totaldevendas2 = Convert.ToDouble(totaldevendas);
+
+                double resultado1 = totaldevendas2 * comissao2 / 100;
+                double resultado2 = resultado1 + salariobase2;
+                Console.WriteLine("Seu salario total e:"+ resultado2);
+
+                Console.WriteLine("Digite S para sair e N para voltar ao inicio");
+
+                String sair = Console.ReadLine();
+                if (sair == "S")
+                {
+
+                    break;
+
+                }
+                if (sair == "N")
+                {
+
+                    continue;
+
+
+                }
+            }
+        }
+    }
+}
