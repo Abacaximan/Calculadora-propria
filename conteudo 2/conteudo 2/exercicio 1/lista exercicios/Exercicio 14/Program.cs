880eab70031f2bc656844c09b35bfb1d60ce2825
gustavo santos <gustavo1912008@gmail.com>
2024-03-14 19:26:50 -0300

    Nome e idade

---
+++ conteudo 2/conteudo 2/exercicio 1/lista exercicios/Exercicio 14/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Exercicio_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Qual e seu nome ?");
+                string nome = "";
+                nome = Console.ReadLine();
+
+                Console.WriteLine("Qual e sua idade ?");
+                string idade = "";
+                idade= Console.ReadLine();
+                double idadenumero = Convert.ToDouble(idade);
+
+                double resultadoidade = idadenumero * 365;
+
+
+                Console.WriteLine(nome + " voce ja viveu " + resultadoidade + " Dias");
+
+
+                Console.WriteLine("Digite S para sair e N para voltar ao inicio");
+
+                String sair = Console.ReadLine();
+
+                if (sair == "S")
+                {
+
+                    break;
+
+                }
+                if (sair == "N")
+                {
+
+                    continue;
+
+
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
