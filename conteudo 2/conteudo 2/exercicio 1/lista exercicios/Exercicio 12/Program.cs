da5853b1a62da9707f9ac884d877d1febbbefa6c
gustavo santos <gustavo1912008@gmail.com>
2024-03-14 19:15:10 -0300

    Exercicio da padaria foi colocado

---
+++ conteudo 2/conteudo 2/exercicio 1/lista exercicios/Exercicio 12/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Exercicio_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          while (true)
+            {
+
+                Console.Clear();
+
+
+                Console.WriteLine("Calculadora para area de terreno");
+
+
+                Console.WriteLine("informe o comprimento do terreno");
+                string comprimentoterreno1 = "";
+                comprimentoterreno1 = Console.ReadLine();
+                double areadoterreno = Convert.ToDouble(comprimentoterreno1);
+
+                Console.WriteLine("informe a largura do terreno ");
+                string larguradoterreno1 = "";
+                larguradoterreno1 = Console.ReadLine();
+                double larguradoterreno = Convert.ToDouble(larguradoterreno1);
+
+                double resultado1 = areadoterreno * larguradoterreno;
+
+
+             Console.WriteLine("a area do terreno e:" +  resultado1 +"M");
+
+
+                Console.WriteLine("Digite S para sair e N para voltar ao inicio");
+
+                String sair = Console.ReadLine();
+
+                if (sair == "S")
+                {
+
+                    break;
+
+                }
+                if (sair == "N")
+                {
+
+                    continue;
+
+
+                }
+
+            }
+        }
+    }
+}
---
+++ conteudo 2/conteudo 2/exercicio 1/lista exercicios/Exercicio 13/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Exercicio_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           while (true) { 
+            
+                Console.Clear();
+
+
+
+
+
+                Console.WriteLine("digite a quantidade de paes que foram vendidos no dia");
+                string quantidadepaes = "";
+                quantidadepaes=Console.ReadLine();
+                double quantidadepaes1 = Convert.ToDouble(quantidadepaes);
+
+                Console.WriteLine("Digite a quantia de broas que foram vendidas no dia");
+                string quantidadebroas = "";
+                quantidadebroas=Console.ReadLine();
+                double quantidadebroas1=Convert.ToDouble(quantidadebroas);
+
+                double resultadopaes = quantidadepaes1 * 0.12;
+                double resultadobroas= quantidadebroas1*1.5;
+
+                double arrecadacao =  resultadopaes + resultadobroas;
+                double poupanca = arrecadacao * 0.1;
+                 
+                Console.WriteLine("a sua arrecadacao do dia foi " + arrecadacao);
+                Console.WriteLine("deve ser investido esta quantia na poupanca "+ poupanca);
+                
+                Console.WriteLine("Digite S para sair e N para voltar ao inicio");
+
+                String sair = Console.ReadLine();
+
+                if (sair == "S")
+                {
+
+                    break;
+
+                }
+                if (sair == "N")
+                {
+
+                    continue;
+
+
+                }
+
+
+            }
+
+
+        }
+    }
+}
