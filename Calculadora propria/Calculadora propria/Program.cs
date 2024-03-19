fd088d07e637196eb32cb14585fb62d3a91aea92
gustavo santos <gustavo1912008@gmail.com>
2024-03-19 16:13:44 -0300

    Separado

--- Calculadora propria/Calculadora propria/Program.cs
+++ Calculadora propria/Calculadora propria/Program.cs
@@ -57,47 +57,29 @@ namespace Calculadora_propria
             Console.WriteLine("Digite 3 para dividir");
             Console.WriteLine("digite 4 para multiplicar");
             Console.WriteLine("Digite S se quiser sair");
-
-            string operacao = Console.ReadLine();
-
+         string operacao = Console.ReadLine();
             return operacao;
-
         }
-
         static bool opcaodesaidaselecionada(string opcao)
         {
-
             bool opcaosaida1 = opcao == "S" || opcao == "s";
-
-
             return opcaosaida1;
-
         }
         static bool opcaoinvalida(string opcao)
         {
-
             bool opcaoinvalida1 = opcao != "S" && opcao != "1" && opcao != "2" && opcao != "3" && opcao != "4" && opcao != "s";
-
-
             return opcaoinvalida1;
         }
-
         static void realizarcalculo(string operacao)
         {
-
             Console.WriteLine();
             Console.WriteLine("Digite o primeiro numero");
-
             double primeironumero = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("digite o segundo numero");
-
             double segundonumero = Convert.ToDouble(Console.ReadLine());
             double resultado = 0;
-
-
             switch (operacao)
             {
-
                 case "1":
                     resultado = primeironumero + segundonumero; break;
 
@@ -106,8 +88,6 @@ namespace Calculadora_propria
 
                 case "3":
                     resultado = primeironumero * segundonumero; break;
-
-
                 case "4":
 
                     while (segundonumero == 0)
@@ -119,9 +99,7 @@ namespace Calculadora_propria
                     resultado = primeironumero / segundonumero;
 
                     break;
-
             }
-
             Exibirresultado(resultado);
         }
         private static void Exibirresultado(double resultado)
