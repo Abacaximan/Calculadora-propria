bd5981e05edd66067fdd9c157213a0b65dedf532
gustavo santos <gustavo1912008@gmail.com>
2024-03-25 20:07:03 -0300

    Terminando o exercicio

--- Calculadora propria/Arrays e funçoes/Program.cs
+++ Calculadora propria/Arrays e funçoes/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Numerics;
+using System.Runtime.InteropServices;
 
 namespace Arrays_e_funçoes
 {
@@ -10,23 +11,39 @@ namespace Arrays_e_funçoes
             Console.ForegroundColor = ConsoleColor.Red;
 
 
-            int[] numeros = new int[] { -5, 3, 4, 5, 9, 6, 10, -2, 11, 1, 2, 6, 7, 8, 0, -6 };
-
+            int[] numeros = new int[] { -5, 3, 4, 5, 9, 6, 10, -2, 11, 1, 2, 6, 7, 8, 0, -6 };           
             int valormaximo = numeros.Max();
+          
 
+
+            Console.WriteLine("A sequencia dos numeros em ordem Decrescente E: ");
+
+            Array.Sort(numeros);
+            Array.Reverse(numeros);
+
+            foreach (var sequencia in numeros)
+            {
+                Console.Write(sequencia + " ");
+
+            }
+
+            Console.ReadLine();
+
+            Console.WriteLine("O valor maximo da sequencia e: ");
             Console.WriteLine(valormaximo);
             Console.ReadLine();
 
+            Console.WriteLine("O valor minimo da sequencia e: ");
             int valorminimo = numeros.Min();
             Console.WriteLine(valorminimo);
             Console.ReadLine();
 
+            Console.WriteLine("O valor medio da sequencia e: ");
             double medio = numeros.Average();
             Console.WriteLine(medio);
             Console.ReadLine();
 
-            Array.Sort(numeros);
-            Array.Reverse(numeros);
+
 
             var tresmaiores = numeros.Take(3);
 
@@ -39,12 +56,25 @@ namespace Arrays_e_funçoes
                 Console.Write( valor + " ");
                 
             }
+            Console.WriteLine();
+            Console.ReadLine();
 
-            Console.ReadLine( );
+            Console.WriteLine("Os numeros negativos sao: ");
+            for (int i = 0;i < numeros.Length;i++)
+            {
+
+                if (numeros[i] < 0)
+                {
+                   
+                    Console.WriteLine($"{numeros[i] + ""}");
 
 
+                }               
+            }
+            Console.ReadLine();
 
-
+            
+           
 
 
 
@@ -53,8 +83,7 @@ namespace Arrays_e_funçoes
 
 
         }    
-
     }
- 
+
 }
 
